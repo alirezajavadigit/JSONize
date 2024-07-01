c1e34abd3242dd97359c98c1f51f9edfc87fb108
alirezajawadi <alirezajavadigit@gmail.com>
2024-07-02 02:00:20 +0330

    :white_check_mark: refactor: improve HasAttribute

--- src/System/Traits/HasAttribute.php
+++ src/System/Traits/HasAttribute.php
@@ -15,9 +15,9 @@
 |--------------------------------------------------------------------------
 | HasAttribute Trait
 |--------------------------------------------------------------------------
-| This trait provides methods for setting message, data, status, and headers attributes 
-| to be included in the response. It encapsulates the response attributes and 
-| allows for easy manipulation and customization.
+| This trait provides methods for setting message, data, status, and headers 
+| attributes to be included in the response. It encapsulates the response attributes 
+| and allows for easy manipulation and customization.
 |--------------------------------------------------------------------------
 */
 
@@ -28,7 +28,7 @@ trait HasAttribute
     /**
      * @var string|null $message Stores the message to be included in the response.
      */
-    private string $message;
+    private ?string $message = null; // Initialize the variable and add nullability
 
     /**
      * @var mixed|null $data Stores the data payload to be included in the response.
@@ -36,14 +36,34 @@ trait HasAttribute
     private $data = null;
 
     /**
-     * @var int $status Stores the HTTP status code for the response.
+     * @var array $status Stores the HTTP status code and message for the response.
      */
-    private $status = 200;
+    private $status = [200, '']; // Change to array to store both code and message
 
     /**
-     * @var array $headers Stores the custom headerss to be included in the response.
+     * @var array $headers Stores the custom headers to be included in the response.
      */
     private $headers = [];
+
+    /**
+     * @var bool $hideStatus Flag to hide the status in the response.
+     */
+    private $hideStatus = false;
+
+    /**
+     * @var bool $hideData Flag to hide the data in the response.
+     */
+    private $hideData = false;
+
+    /**
+     * @var bool $hideMessage Flag to hide the message in the response.
+     */
+    private $hideMessage = false;
+
+    /**
+     * @var bool $hideSuccess Flag to hide the success status in the response.
+     */
+    private $hideSuccess = false;
 
     /**
      * Set the message to be included in the response.
@@ -61,22 +81,23 @@ trait HasAttribute
     /**
      * Get the message included in the response.
      *
-     * @return string The message included in the response.
+     * @return string|null The message included in the response.
      */
-    public function getMessage()
+    public function getMessage(): ?string // Add return type hint and nullability
     {
-        return isset($this->message) ? $this->message : null;
+        return $this->message;
     }
 
     /**
      * Set the data payload to be included in the response.
      *
      * @param mixed $data The data payload to be included in the response.
+     * @param mixed $key The key to store the data payload under.
      * @return $this The current instance for method chaining.
      */
-    public function setData($data)
+    public function setData($data, mixed $key = "data")
     {
-        $this->data = $data;
+        $this->data = [$data, $key];
 
         return $this;
     }
@@ -92,24 +113,25 @@ trait HasAttribute
     }
 
     /**
-     * Set the HTTP status code for the response.
+     * Set the HTTP status code and message for the response.
      *
      * @param int $status The HTTP status code for the response.
+     * @param string $message The HTTP status message for the response.
      * @return $this The current instance for method chaining.
      */
-    public function setStatus(int $status)
+    public function setStatus(int $status, string $message = "")
     {
-        $this->status = $status;
+        $this->status = [$status, $message];
 
         return $this;
     }
 
     /**
-     * Get the HTTP status code for the response.
+     * Get the HTTP status code and message for the response.
      *
-     * @return int The HTTP status code for the response.
+     * @return array The HTTP status code and message for the response.
      */
-    public function getStatus()
+    public function getStatus(): array // Add return type hint
     {
         return $this->status;
     }
@@ -132,8 +154,96 @@ trait HasAttribute
      *
      * @return array The custom headers included in the response.
      */
-    public function getHeaders()
+    public function getHeaders(): array // Add return type hint
     {
         return $this->headers;
+    }
+
+    /**
+     * Get the value of hideStatus.
+     *
+     * @return bool The value of hideStatus.
+     */
+    public function getHideStatus(): bool // Add return type hint
+    {
+        return $this->hideStatus;
+    }
+
+    /**
+     * Set the value of hideStatus.
+     *
+     * @return $this The current instance for method chaining.
+     */
+    public function setHideStatus()
+    {
+        $this->hideStatus = true;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hideData.
+     *
+     * @return bool The value of hideData.
+     */
+    public function getHideData(): bool // Add return type hint
+    {
+        return $this->hideData;
+    }
+
+    /**
+     * Set the value of hideData.
+     *
+     * @return $this The current instance for method chaining.
+     */
+    public function setHideData()
+    {
+        $this->hideData = true;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hideMessage.
+     *
+     * @return bool The value of hideMessage.
+     */
+    public function getHideMessage(): bool // Add return type hint
+    {
+        return $this->hideMessage;
+    }
+
+    /**
+     * Set the value of hideMessage.
+     *
+     * @return $this The current instance for method chaining.
+     */
+    public function setHideMessage()
+    {
+        $this->hideMessage = true;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of hideSuccess.
+     *
+     * @return bool The value of hideSuccess.
+     */
+    public function getHideSuccess(): bool // Add return type hint
+    {
+        return $this->hideSuccess;
+    }
+
+    /**
+     * Set the value of hideSuccess.
+     *
+     * @return $this The current instance for method chaining.
+     */
+    public function setHideSuccess()
+    {
+        $this->hideSuccess = true;
+
+        return $this;
     }
 }
