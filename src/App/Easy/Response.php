97794cf38a73cf9ce70afced804d4e5ca53bbc17
alireza javadi <alirezajavadigit@gmail.com>
2025-03-29 07:12:04 +0330

    :white_check_mark: feat(response): add get() method and conditional output in destructor

--- src/App/Easy/Response.php
+++ src/App/Easy/Response.php
@@ -20,22 +20,31 @@
 |--------------------------------------------------------------------------
 */
 
-namespace JSONize\App\Easy; //Namespace declaration for the JSONize\App package
+namespace JSONize\App\Easy;
 
 use JSONize\System\Traits\HasAttribute;
 use JSONize\System\Traits\Easy\MethodCaller;
 use JSONize\System\Traits\HasStatus;
-use JSONize\System\Traits\HasStructure; // Importing the HasStructure trait from the JSONize\System\Traits namespace
+use JSONize\System\Traits\HasStructure;
 
 class Response
 {
     use MethodCaller, HasStatus, HasAttribute, HasStructure;
-
-    public function __destruct()
+    private $isGeted = false;
+    public function get()
     {
+        $this->isGeted = true;
         header('Content-Type: application/json'); // Set content type header to JSON
         $this->extractHeaders(); // Extract and set custom headers
-        echo $this->makeReturnableJson(); // Format response data into JSON and return
-        exit;
+        return $this->makeReturnableJson(); // Format response data into JSON and return
+    }
+    public function __destruct()
+    {
+        if (!$this->isGeted) {
+            header('Content-Type: application/json'); // Set content type header to JSON
+            $this->extractHeaders(); // Extract and set custom headers
+            echo $this->makeReturnableJson(); // Format response data into JSON and return
+            exit;
+        }
     }
 }
