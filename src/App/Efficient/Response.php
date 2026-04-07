88724d1d526051d690932c667c8d88c1d33179fe
alirezajavadigit <alirezajavadigit@users.noreply.github.com>
2026-04-07 12:41:19 +0330

    refactor(efficient): improve Response implementation

--- src/App/Efficient/Response.php
+++ src/App/Efficient/Response.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| JSONize Library
-|--------------------------------------------------------------------------
-| Standardize JSON for cleaner, consistent APIs, web, and mobile apps.
-|--------------------------------------------------------------------------
-| @category  Library
-| @package   JSONize
-| @version   1.8.1
-| @author    Alireza Javadi
-| @license   MIT License
-| @link      https://github.com/alirezajavadigit/JSONize
-|--------------------------------------------------------------------------
-| Response Class
-|--------------------------------------------------------------------------
-| This class handles the response formatting and ensures consistency in JSON output.
-| Utilizes Singleton and MethodCaller traits.
-|--------------------------------------------------------------------------
-*/
-
 namespace JSONize\App\Efficient;
 
 use JSONize\System\Traits\HasAttribute;
@@ -31,4 +11,11 @@ use JSONize\System\Traits\HasStructure;
 class Response
 {
     use Singleton, MethodCaller, HasStatus, HasAttribute, HasStructure;
+
+    public function get(): string
+    {
+        header('Content-Type: application/json');
+        $this->extractHeaders();
+        return $this->makeReturnableJson();
+    }
 }
